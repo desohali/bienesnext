ca876255fdea788add829de65923fcea26806a0d
hamer orlando albarran valderrama <hameralbarran@gmail.com>
2023-12-11 12:32:53 -0500

    al fin qr

--- src/app/qr/page.tsx
+++ src/app/qr/page.tsx
@@ -1,45 +1,58 @@
 "use client";
 import React, { useRef, useEffect } from 'react';
-/* import Quagga from 'quagga'; */
-const Quagga = require('quagga')
-const BarcodeScanner = () => {
-  const videoRef = useRef(null);
+import { BrowserMultiFormatReader } from '@zxing/library';
 
+const Scanner = () => {
+  const videoRef = useRef<any>(null);
+  const codeReader = useRef(new BrowserMultiFormatReader());
+  const [text, settext] = React.useState<string>("second");
   useEffect(() => {
-    Quagga.init(
-      {
-        inputStream: {
-          name: 'Live',
-          type: 'LiveStream',
-          target: videoRef.current,
-          constraints: {
-            facingMode: 'environment', // Puede ser 'user' para la cámara frontal
-          },
-        },
-        decoder: {
-          readers: ['code_128_reader'], // Puedes especificar diferentes tipos de lectores aquí
-        },
-      },
-      (err:any) => {
-        if (err) {
-          console.error('Error al inicializar Quagga:', err);
-          return;
-        }
-        Quagga.start();
-      }
-    );
+    let scanning = true;
 
-    Quagga.onDetected((data:any) => {
-      console.log('Código de barras detectado:', data);
-      // Aquí puedes manejar la información del código de barras detectado
-    });
+    codeReader.current.listVideoInputDevices()
+      .then((videoInputDevices) => {
+        if (videoInputDevices && videoInputDevices.length > 0) {
+          // Iniciar el escaneo continuo
+          const startScanning = () => {
+            codeReader.current.decodeFromInputVideoDevice(videoInputDevices[0].deviceId, videoRef.current)
+              .then((result:any) => {
+                console.log('Código leído:', result.text);
+                // Hacer algo con el código leído
+                settext(result.text)
+                if (scanning) {
+                  startScanning(); // Reiniciar el escaneo para leer el siguiente código
+                }
+              })
+              .catch((err) => {
+                console.error('Error de lectura:', err);
+                if (scanning) {
+                  startScanning(); // Intentar leer el siguiente código en caso de error
+                }
+              });
+          };
+
+          startScanning(); // Comenzar el escaneo continuo
+        } else {
+          console.error('No se encontraron cámaras disponibles.');
+        }
+      })
+      .catch((err) => {
+        console.error('Error al listar cámaras:', err);
+      });
 
     return () => {
-      Quagga.stop();
+      scanning = false; // Detener el escaneo al desmontar el componente
+      codeReader.current.reset();
     };
   }, []);
 
-  return <video ref={videoRef} />;
+  return <>
+  <video ref={videoRef} />
+  <h4>{text}</h4>
+  </>;
 };
 
-export default BarcodeScanner;
+export default Scanner;
+
+
+
